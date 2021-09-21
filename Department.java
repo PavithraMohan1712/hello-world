7bb34ae486327c2c536332bfc1a75b7444bfc296
P00ja-Sri <89896899+P00ja-Sri@users.noreply.github.com>
2021-09-21 13:44:24 +0530

    Update Department.java

--- Department.java
+++ Department.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-@Entities
+@Entity
 @Table(name="department")
 public class Department implements Serializable{
     private static final long serialVersionUID=1L;
